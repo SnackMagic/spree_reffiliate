cd1f94692c627f3bb945545fa060ba9ab407b8c4
Akshat Singhal <akshat@vinsol.com>
2022-05-17 19:10:40 +0530

    Comment controllers

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,15 +1,15 @@
-Spree::Api::V1::CheckoutsController.class_eval do
-  before_action :set_affilate, only: :update
-  after_action :clear_session, only: :update
+# Spree::CheckoutController.class_eval do
+#   before_action :set_affilate, only: :update
+#   after_action :clear_session, only: :update
 
-  private
-    def set_affilate
-      if @order.payment? && permitted_checkout_attributes[:affiliate]
-        @order.affiliate = Spree::Affiliate.find_by(path: permitted_checkout_attributes[:affiliate])
-      end
-    end
+#   private
+#     def set_affilate
+#       if @order.payment? && session[:affiliate]
+#         @order.affiliate = Spree::Affiliate.find_by(path: session[:affiliate])
+#       end
+#     end
 
-    def clear_session
-      session[:affiliate] = nil if @order.completed?
-    end
-end
+#     def clear_session
+#       session[:affiliate] = nil if @order.completed?
+#     end
+# end
