9af11c61bcd699c393efe6985c3b835c9134b228
Akshat Singhal <akshat@vinsol.com>
2022-05-18 17:19:40 +0530

    Fix migration version

--- db/migrate/20220518104010_seed_commission_rules.rb
+++ db/migrate/20220518104010_seed_commission_rules.rb
@@ -1,4 +1,4 @@
-class SeedCommissionRules < ActiveRecord::Migration[5.2]
+class SeedCommissionRules < ActiveRecord::Migration[5.1]
   def up
     Spree::CommissionRule.find_or_create_by(name: Spree::CommissionRule::USER_REGISTRATION, fixed_commission: true)
     Spree::CommissionRule.find_or_create_by(name: Spree::CommissionRule::ORDER_PLACEMENT, fixed_commission: false)
